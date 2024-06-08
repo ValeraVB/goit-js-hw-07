fa99ea720a43e0e7d7fcc7394bf8645e8a485f6c
Valeriy <vb@profish.com.ua>
2024-06-08 22:07:03 +0300

    home work 7

---
+++ js/task-6.js
@@ -0,0 +1,43 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const input = document.querySelector("input");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const boxesContainer = document.getElementById("boxes");
+
+createBtn.addEventListener("click", createBoxes);
+destroyBtn.addEventListener("click", destroyBoxes);
+
+function createBoxes() {
+  const amount = Number(input.value);
+
+  if (amount < 1 || amount > 100) {
+    return;
+  }
+
+  clearBoxes();
+
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+    size += 10;
+  }
+
+  input.value = "";
+}
+
+function clearBoxes() {
+  boxesContainer.innerHTML = "";
+}
+
+function destroyBoxes() {
+  clearBoxes();
+}
