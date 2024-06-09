41a91d39488ece6cc3fa161f748c4b3e3a50e45f
Valeriy <vb@profish.com.ua>
2024-06-09 23:55:30 +0300

    home work 7 v1

--- js/task-6.js
+++ js/task-6.js
@@ -1,7 +1,7 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, "0")}`;
 }
 
 const input = document.querySelector("input");
@@ -21,16 +21,18 @@ function createBoxes() {
 
   clearBoxes();
 
+  const fragment = document.createDocumentFragment();
   let size = 30;
   for (let i = 0; i < amount; i++) {
     const box = document.createElement("div");
     box.style.width = `${size}px`;
     box.style.height = `${size}px`;
     box.style.backgroundColor = getRandomHexColor();
-    boxesContainer.appendChild(box);
+    fragment.appendChild(box);
     size += 10;
   }
 
+  boxesContainer.appendChild(fragment);
   input.value = "";
 }
 
